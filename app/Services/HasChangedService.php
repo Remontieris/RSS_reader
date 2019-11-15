9d1b934b31d3dea2c0a222629d84abc984623bf0
Robis <robertskemps@gmail.com>
2019-11-15 11:32:27 +0200

    Create new feed that cheks if feed has changed and implement it in RssService

---
+++ app/Services/HasChangedService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+class HasChangedService
+{
+ /**
+     * Check if feed has been updated since last request
+     *
+     * @param string $feed RSS feed as string
+     * @return boolean returns true or false
+     */
+    private function hasFeedChanged($feed = null)
+    {
+        if ($feed == null) {
+            return true;
+        }
+
+        $last_saved_update = LastUpdated::orderBy('id', 'DESC')->first();
+
+        if (empty($last_saved_update)) {
+            return true;
+        }
+
+        $feed = simplexml_load_string($feed);
+        $this->updated = (string) $feed->updated;
+
+        if ($last_saved_update->rss_updated == $this->updated) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+}
--- app/Services/RssService.php
+++ app/Services/RssService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\LastUpdated;
 use GuzzleHttp\Client;
 use App\Services\WordSortService;
+use App\Services\HasChangedService;
 
 class RssService
 {
@@ -16,7 +17,9 @@ class RssService
         $read_url_service = new ReadUrlService();
         $unsanitized_feed = $read_url_service->returnContentFromUrl($this->feed_url);
 
-        if ($unsanitized_feed == null || !$this->wasChanged($unsanitized_feed)) {
+        $changed_service = new HasChangedService();
+
+        if ($unsanitized_feed == null || !$changed_service->hasFeedChanged($unsanitized_feed)) {
             return null;
         }
 
@@ -24,33 +27,5 @@ class RssService
         $sorted_feed = $sort_array->returnSortedArray($unsanitized_feed);
 
         return $sorted_feed;
-    }
-
-    /**
-     * Check if feed has been updated since last request
-     *
-     * @param string $feed RSS feed as string
-     * @return boolean returns true or false
-     */
-    private function wasChanged($feed = null)
-    {
-        if ($feed == null) {
-            return true;
-        }
-
-        $last_saved_update = LastUpdated::orderBy('id', 'DESC')->first();
-
-        if (empty($last_saved_update)) {
-            return true;
-        }
-
-        $feed = simplexml_load_string($feed);
-        $this->updated = (string) $feed->updated;
-
-        if ($last_saved_update->rss_updated == $this->updated) {
-            return false;
-        }
-
-        return true;
     }
 }
